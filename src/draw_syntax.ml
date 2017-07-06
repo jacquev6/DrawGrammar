dced07721746b89795b1bb982c0af1792e882ef4
Vincent Jacques <vincent@vincent-jacques.net>
2017-07-06 18:33:08 +0200

    Draw something on the canvas!

--- src/draw_syntax.ml
+++ src/draw_syntax.ml
@@ -1,5 +1,14 @@
 open General.Abbr
 
-module Printf = OCamlStandard.Printf
+let draw_syntax_on_canvas syntax canvas =
+    let get_by_id id coerce = Js.Opt.get (coerce (Dom_html.getElementById id)) (fun () -> raise Not_found) in
+    let canvas = get_by_id canvas Dom_html.CoerceTo.canvas in
+    let context = canvas##getContext (Dom_html._2d_) in
+    begin
+      context##moveTo (0., 0.);
+      context##lineTo (100., 100.);
+      context##stroke ();
+      context##fillText (syntax, 25., 25.);
+    end
 
-let () = Printf.printf "Hello, World!\n"
+let () = Js.Unsafe.global##draw_syntax_on_canvas_ (* why do I need a last undesrcore? *) <- Js.wrap_callback draw_syntax_on_canvas
